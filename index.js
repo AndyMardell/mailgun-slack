75fe03b14e76ce0a8ffa29068b75342937539a7c
Andy Mardell <andy@sidigital.co>
2019-11-19 21:34:26 +0000

    verify token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 const generateResponse = require('./lib/generateResponse')
+const verifyKey = require('./lib/verifyKey')
 
 exports.handler = async (event, context, callback) => {
   const body = JSON.parse(event.body)
@@ -8,6 +9,7 @@ exports.handler = async (event, context, callback) => {
   console.log(body)
 
   try {
+    verifyKey(body.signature)
     await axios.post(process.env.SLACK_WEBHOOK, {
       text: `There was a problem sending mail to ${data.recipient}
         from ${data.message.headers.from}. Event: ${data.event}`

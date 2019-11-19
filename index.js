80e5854ab787b91e2ec5666d3e713aa63e008015
Andy Mardell <andy@sidigital.co>
2019-11-19 21:12:35 +0000

    Generate response properly

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const axios = require('axios')
+const generateResponse = require('generateResponse')
 
 exports.handler = async (event, context, callback) => {
   const mailgunEvent = JSON.parse(event.body)
   const mailgunError = mailgunEvent['event-data']
 
   try {
-    await axios.post(
-      '***REMOVED***',
-      {
-        text: `There was a problem sending mail to ${mailgunError.recipient}
+    await axios.post(process.env.SLACK_WEBHOOK, {
+      text: `There was a problem sending mail to ${mailgunError.recipient}
         from ${mailgunError.message.headers.from}. Event: ${mailgunError.event}`
-      }
-    )
-    console.log('No error')
-    callback(null, 'OK')
+    })
+    const response = generateResponse({ result: 'OK' }, 200)
+    callback(null, response)
   } catch (err) {
-    console.log('error', err)
     throw new Error(err)
   }
 }

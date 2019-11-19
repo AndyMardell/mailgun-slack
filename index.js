741b698fdc3c468bf7b09673ad7e696eba40591d
Andy Mardell <andy@sidigital.co>
2019-11-19 21:01:37 +0000

    Axios

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-const https = require('https')
+const axios = require('axios')
 
 exports.handler = (event, context, callback) => {
   const mailgunEvent = JSON.parse(event.body)
   const mailgunError = mailgunEvent['event-data']
 
-  const payload = JSON.stringify({
-    text: `There was a problem sending mail to ${mailgunError.recipient} from
-      ${mailgunError.message.headers.from}. Event: ${mailgunError.event}`
-  })
-
-  const options = {
-    hostname: 'hooks.slack.com',
-    method: 'POST',
-    path: '***REMOVED***'
+  try {
+    axios.post(
+      '***REMOVED***',
+      {
+        text: `There was a problem sending mail to ${mailgunError.recipient}
+        from ${mailgunError.message.headers.from}. Event: ${mailgunError.event}`
+      }
+    )
+    callback(null, 'OK')
+  } catch (err) {
+    throw new Error(err)
   }
-
-  const req = https.request(options, res =>
-    res.on('data', () => callback(null, 'OK'))
-  )
-  req.on('error', error => callback(JSON.stringify(error)))
-  req.write(payload)
-  req.end()
 }

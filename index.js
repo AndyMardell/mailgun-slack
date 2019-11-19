f54b83dfa10dffbe5bff3f5e64c5ae7a533777b1
Andy Mardell <andy@sidigital.co>
2019-11-19 21:06:37 +0000

    async

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const axios = require('axios')
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
   const mailgunEvent = JSON.parse(event.body)
   const mailgunError = mailgunEvent['event-data']
 
   try {
-    axios.post(
+    await axios.post(
       '***REMOVED***',
       {
         text: `There was a problem sending mail to ${mailgunError.recipient}

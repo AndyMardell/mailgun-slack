a092b809ce0e06a29a0812f65a84bebf2edd094a
Andy Mardell <andy@sidigital.co>
2019-11-19 21:14:41 +0000

    Bump

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const axios = require('axios')
-const generateResponse = require('generateResponse')
+const generateResponse = require('../lib/generateResponse')
 
 exports.handler = async (event, context, callback) => {
-  const mailgunEvent = JSON.parse(event.body)
-  const mailgunError = mailgunEvent['event-data']
+  const body = JSON.parse(event.body)
+  const data = body['event-data']
 
   try {
     await axios.post(process.env.SLACK_WEBHOOK, {
-      text: `There was a problem sending mail to ${mailgunError.recipient}
-        from ${mailgunError.message.headers.from}. Event: ${mailgunError.event}`
+      text: `There was a problem sending mail to ${data.recipient}
+        from ${data.message.headers.from}. Event: ${data.event}`
     })
     const response = generateResponse({ result: 'OK' }, 200)
     callback(null, response)
